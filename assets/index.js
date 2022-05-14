690b1ee694dee9f8561bb8bc57a58d9d67433cee
Dennis Mwangi <dennis.kamau@student.moringaschool.com>
2022-05-14 15:28:03 +0300

    created a style.css and index.js file and put the files in an assets folder.i linked the two files to the index.html.

---
+++ assets/index.js
@@ -0,0 +1,13 @@
+const questions = [
+    {
+        question:"What does HTML stand for?",
+        a:"Hyper Text Multi Language",
+        b:"Heavy Text Markup Language",
+        c:"Hyper Text Mark Language",
+        d:"Hyper Text Markup Language",
+        correct:d
+    }
+
+    
+]
+document.getElementsByClassName("optionA").innerHTML = "Hyper";
