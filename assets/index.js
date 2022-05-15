44ea3aaeb33a126a7456b189582d0d957d3e5f45
Dennis Mwangi <dennis.kamau@student.moringaschool.com>
2022-05-15 06:41:47 +0300

    added questions to be displayed on the quiz board

--- assets/index.js
+++ assets/index.js
@@ -1,13 +1,68 @@
-const questions = [
+ /*function loading(){
+    alert("Welcome to Get Quizy.")
+}*/
+function name(){
+    let username = prompt("Please enter your name")
+    if(username != null){
+        document.getElementById("username").innerHTML=username
+    }else{
+        document.getElementById("username").innerText="player1"
+    }
+}
+function date(){
+   alert(Date) 
+}
+
+const quizQuestions=[
     {
         question:"What does HTML stand for?",
-        a:"Hyper Text Multi Language",
-        b:"Heavy Text Markup Language",
+        a:"Heavy Text Material Language",
+        b:"Hyper Type Markup Language",
         c:"Hyper Text Mark Language",
         d:"Hyper Text Markup Language",
         correct:d
+    },
+    {
+        question:"What does CSS stand for?",
+        a:"Cascading Still Sheets",
+        b:"Cascading Style Sheets",
+        c:"Cascading Simple Sheets",
+        d:"It doesn't stand for anything",
+        correct:b 
+    },
+    {
+        question:"Which of these is not a css selector?",
+        a:"class selector",
+        b:"Id selector",
+        c:"universal selector",
+        d:"style selector",
+        correct:d
+    },
+    {
+        question:"How many divs can one have in an HTML document?",
+        a:"less than 5",
+        b:"less than 10",
+        c:"As many as one please",
+        d:"only 1",
+        correct:c 
+    },
+    {
+        question:"When was javascript created?",
+        a:"1995",
+        b:"1996",
+        c:"1994",
+        d:"1997",
+        correct:a
     }
 
-    
 ]
-document.getElementsByClassName("optionA").innerHTML = "Hyper";
+
+const quiz = document.getElementById("quiz")
+const questionEl = document.getElementById("question")
+const answerEl = document.querySelectorAll(".answer")
+const textA = document.getElementById("textA")
+const textB = document.getElementById("textB")
+const textC = document.getElementById("textC")
+const textD = document.getElementById("textD")
+const submitbtn = document.getElementById("submit")
+
